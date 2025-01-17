c92d7864858e757fab320470d2a9ffc8ae92d96b
Birmadu Wakessa <b.wakessa@mail.utoronto.ca>
2025-01-17 14:11:54 -0500

    currently implementing note pairing algorithm

--- changelog_entities/changelog.py
+++ changelog_entities/changelog.py
@@ -1,8 +1,9 @@
 from pyflp import Project
 import enum
+from itertools import zip_longest
 
 @enum.unique
-class ChangeType(enum):
+class ChangeType(enum.Enum):
     INSERT = 1
     DELETE = 2
     POSITION = 3
@@ -27,6 +28,44 @@ class ChangeParser:
         pass
 
     def parse_changes(self, v1: Project, v2: Project) -> ChangeLog:
-        pass
+        changelog = ChangeLog()
+
+        for pattern1, pattern2 in zip(v1.patterns, v2.patterns):
+            # Must exhuast the generators into a data structure to determine insertions/deletions, etc.
+            # Possible data structures:
+            # - indexed adjacent arrays (2x space of data)
+            # - a hash table (1x-2x space of data, but cant accomodate for different note counts)
+            # Choice: indexed adjacent arrays (avoids possible overhead of hash tables)
+
+            unmatched_indices = [] # indices of notes in <v1_notes>, <v2_notes> where changes between notes occurred
+
+            v1_notes = []
+            v2_notes = []
+            for v1_note, v2_note in zip_longest(pattern1.notes, pattern2.notes, fillvalue=None):
+                if v1_note != v2_note:
+                    unmatched_indices.append(max(len(v1_notes), len(v2_notes)))
+
+                v1_notes.append(v1_note)
+                v2_notes.append(v2_note)
+
+            if len(v1_notes) > len(v2_notes):
+                # deletion(s) occurred: m = len(v1_notes) - len(unmatched_indices) (REFER TO DOCUMENTATION FOR DEFINITION OF <m>)
+                pass
+                # step 1: log  DELETE ChangeEntry
+            elif len(v1_notes) < len(v2_notes):
+                # insertion(s) occurred: m = len(v2_notes) - len(unmatched_indices)
+                pass
+            else:
+                # len(v1) == len(v2)
+                pass
+
+
+
+
+
+
+        return changelog
+
+
 
 
--- save_client.py
+++ save_client.py
@@ -12,7 +12,13 @@ change_parser = ChangeParser()
 
 if __name__ == "__main__":
     # take as input an fl studio file (chosen by the client through file explorer prompt)
+
+    #desktop
     project_path = "C:\\Users\\wbirm\\OneDrive\\Desktop\\Folders\\beats\\fls\\dark melody drill.flp"
+
+    #laptop
+    project_path = "C:\\Users\\wbirm\\OneDrive\\Desktop\\dark melody drill.flp"
+
     project = pyflp.parse(project_path)
 
     v1 = None
@@ -29,6 +35,14 @@ if __name__ == "__main__":
         time.sleep(1)
         v2 = pyflp.parse(project_path) # --------------------------------------- retrieve new version
 
+        for pattern1, pattern2 in zip(v1.patterns, v2.patterns):
+            notes1 = []
+            notes2 = []
+            for note1, note2 in zip(pattern1.notes, pattern2.notes):
+                notes1.append(note1)
+                notes2.append(note2)
+                a = len(pattern1.notes)
+                b = 3
         # TODO: Determine how often to periodically serialize the changelog data to be retrieved in java and sent to the server
 
 
