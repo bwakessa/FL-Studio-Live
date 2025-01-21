4f6de9d796c0bcf845ad38c9687d28ee32450b1d
Birmadu Wakessa <b.wakessa@mail.utoronto.ca>
2025-01-21 18:15:56 -0500

    idk

--- save_client.py
+++ save_client.py
@@ -14,18 +14,17 @@ if __name__ == "__main__":
     # take as input an fl studio file (chosen by the client through file explorer prompt)
 
     #desktop
-    project_path = "C:\\Users\\wbirm\\OneDrive\\Desktop\\Folders\\beats\\fls\\dark melody drill.flp"
+    # project_path = "C:\\Users\\wbirm\\OneDrive\\Desktop\\Folders\\beats\\fls\\dark melody drill.flp"
 
     #laptop
-    #project_path = "C:\\Users\\wbirm\\OneDrive\\Desktop\\dark melody drill.flp"
-
-
+    project_path = "C:\\Users\\wbirm\\OneDrive\\Desktop\\dark melody drill.flp"
     project = pyflp.parse(project_path)
 
     v1 = None
     v2 = project
-
     while True: # TODO: figure out what should trigger the end of this loop
+                # ^ This loop should be active only when a something (like a button) is triggered by the user.
+
         v1 = v2 # -------------------------------------------------------------- shift the most recent version back to v1
         time.sleep(1) # -------------------------------------------------------- length of save period
 
@@ -39,6 +38,7 @@ if __name__ == "__main__":
         change_log_engine.parse_changes(v1, v2)
         # TODO: Determine how often to periodically serialize the changelog data to be retrieved in java and sent to the server
 
+        
         # time.sleep(x)
         # change_log = change_parser.get_changelog()
 
